d9c03f9f653416fcd8072dba6e9fb034332a7b98
Camale H <1249185110@qq.com>
2025-06-29 19:47:27 +0800

    use vite. Add a simple keyboard template.

--- src/components/keyboard_frame.ts
+++ src/components/keyboard_frame.ts
@@ -1,11 +1,16 @@
-function std_keyconfig() {
+export type KeyConfig = {
+  text1: string;
+  text2: string;
+};
+
+export function std_keyconfig(): KeyConfig[] {
   const T1 = ["Q", "D", "R", "W", "B", "J", "F", "U", "P", ":",
-              "A", "S", "H", "T", "G", "Y", "N", "E", "O", "I",
-              "Z", "X", "M", "C", "V", "K", "L", "<", ">", "?"];
+    "A", "S", "H", "T", "G", "Y", "N", "E", "O", "I",
+    "Z", "X", "M", "C", "V", "K", "L", "<", ">", "?"];
   const T2 = [" ", " ", " ", " ", " ", " ", " ", " ", " ", ";",
-              " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-              " ", " ", " ", " ", " ", " ", " ", ",", ".", "/"];
-  const configs = [];
+    " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+    " ", " ", " ", " ", " ", " ", " ", ",", ".", "/"];
+  const configs: KeyConfig[] = [];
   for (let i = 0; i < 30; i++) {
     const config = { text1: T1[i], text2: T2[i] };
     configs.push(config);
@@ -13,7 +18,7 @@ function std_keyconfig() {
   return configs;
 }
 
-function createKeyBoard(keyconfig) {
+export function createKeyBoard(keyconfig: KeyConfig[]): SVGSVGElement{
   const svgNS = "http://www.w3.org/2000/svg"
   const top = document.createElementNS(svgNS, "svg");
   top.setAttribute("x", "0");
@@ -26,41 +31,41 @@ function createKeyBoard(keyconfig) {
   let y = 0;
   for (let i = 0; i < 30; i++) {
     const key = document.createElementNS(svgNS, "rect");
-    key.setAttribute("x", x);
-    key.setAttribute("y", y);
-    key.setAttribute("width", "38");
-    key.setAttribute("height", "38");
+    key.setAttribute("x", x.toString());
+    key.setAttribute("y", y.toString());
+    key.setAttribute("rx", "8");
+    key.setAttribute("ry", "8");
+    key.classList.add("default-key");
     top.appendChild(key);
 
     const text1 = document.createElementNS(svgNS, "text");
-    text1.setAttribute("x", (x+10));
-    text1.setAttribute("y", (y+12));
-    text2.setAttribute("text-anchor", "middle");
-    text2.setAttribute("dominant-baseline", "middle");
-    text2.setAttribute("direction", "ltr");
-    text2.textContent = keyconfig[i].text1;
-    top.appendChild(key);
+    text1.setAttribute("x", (x + 10).toString());
+    text1.setAttribute("y", (y + 12).toString());
+    text1.setAttribute("text-anchor", "middle");
+    text1.setAttribute("dominant-baseline", "middle");
+    text1.setAttribute("direction", "ltr");
+    text1.textContent = keyconfig[i].text1;
+    top.appendChild(text1);
 
-    const text1 = document.createElementNS(svgNS, "text");
-    text1.setAttribute("x", (x+10));
-    text1.setAttribute("y", (y+27));
+    const text2 = document.createElementNS(svgNS, "text");
+    text2.setAttribute("x", (x + 10).toString());
+    text2.setAttribute("y", (y + 27).toString());
     text2.setAttribute("text-anchor", "middle");
     text2.setAttribute("dominant-baseline", "middle");
     text2.setAttribute("direction", "ltr");
     text2.textContent = keyconfig[i].text2;
-    top.appendChild(key);
+    top.appendChild(text2);
 
-    if(i==9) {
+    if (i == 9) {
       x = 10;
       y = 40;
-    } else if(i==19) {
+    } else if (i == 19) {
       x = 30;
       y = 80;
     } else {
       x += 40;
     }
   }
-
-
+  return top;
 }
 
--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,10 @@
 // async function work() {
 //   let frequence = await frequencePromise;
 // }
+import { std_keyconfig, createKeyBoard } from './components/keyboard_frame.ts'
+
+const std_keyboard = createKeyBoard(std_keyconfig());
+
+document.body.append(std_keyboard);
+
+console.log("from typescript");
